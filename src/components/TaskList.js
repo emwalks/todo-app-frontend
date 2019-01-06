4837cab5848cc8bce82b9b5cd880b8d5828b7e1d
emwalks <walkere@tcd.ie>
2019-01-06 18:19:26 +0000

    tasks now represented by an array which is mapped end of lect 030119

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,15 +4,33 @@ import Task from './Task';
 
 class TaskList extends React.Component {
    render(){
-       return (
-           <div>
-               <Task taskDescription="Learn how to drive" />
-               <Task taskDescription="Buy a Van" />
-               <Task taskDescription="Convert Van..." />
-               <Task taskDescription="Van Life!" />
-           </div>
-            
-       );  
+
+    let listOfTasks = [
+        "Learn how to drive",
+        "Buy a Van",
+        "Convert Van...",
+        "Van Life!"
+    ];
+
+    return (
+        <div>
+            {
+                listOfTasks.map((task, i) =>
+                    <Task taskDescription={task} key={i} />
+                )
+            }
+        </div>
+    );
+
+//       return (
+//           <div>
+//               <Task taskDescription="Learn how to drive" />
+//               <Task taskDescription="Buy a Van" />
+//               <Task taskDescription="Convert Van..." />
+//               <Task taskDescription="Van Life!" />
+//           </div>
+//           
+//       );  
    } 
 }
 
