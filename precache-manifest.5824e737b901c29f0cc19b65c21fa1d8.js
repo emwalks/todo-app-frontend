5467fccfce34c3609b030e2ac0daa6f8b905f718
emwalks <walkere@tcd.ie>
2019-03-16 13:49:26 +0000

    Updates

---
+++ precache-manifest.5824e737b901c29f0cc19b65c21fa1d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc6cf831953675f8fbff",
+    "url": "/todo-app-frontend/static/js/runtime~main.cc6cf831.js"
+  },
+  {
+    "revision": "ef327cd797baae9ffb60",
+    "url": "/todo-app-frontend/static/js/main.ef327cd7.chunk.js"
+  },
+  {
+    "revision": "a2252b08af7fe3500739",
+    "url": "/todo-app-frontend/static/js/1.a2252b08.chunk.js"
+  },
+  {
+    "revision": "ef327cd797baae9ffb60",
+    "url": "/todo-app-frontend/static/css/main.0cc9c60e.chunk.css"
+  },
+  {
+    "revision": "cc90adac9b762f79c5a9503caacf19ca",
+    "url": "/todo-app-frontend/index.html"
+  }
+];
